705b9b8b2a9765642da6576f91837b598cf76d1d
artmakhalin <artmakhalin@gmail.com>
2026-01-06 19:19:17 -0500

    champions added

--- src/components/Champion.jsx
+++ src/components/Champion.jsx
@@ -1,14 +1,16 @@
 const Champion = ({ name, imgUrl, years, country }) => {
   return (
-    <div className="card">
-        <img src={imgUrl} className="card-img-top" alt="Champion" />
+    <div className="col">
+      <div className="card h-100">
+        <img src={imgUrl} className="card-img-top" alt={name} />
         <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <h6 className="card-subtitle mb-2 text-body-secondary">{years}</h6>
-            <p className="card-text">{country}</p>
+          <h5 className="card-title">{name}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">{years}</h6>
+          <p className="card-text">{country}</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Champion
+export default Champion;
